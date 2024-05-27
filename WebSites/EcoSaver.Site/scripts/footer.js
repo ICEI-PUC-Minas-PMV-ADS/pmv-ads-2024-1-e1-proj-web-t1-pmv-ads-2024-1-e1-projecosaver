b6ce1c7f73de22373ffeb81c95a71207c90caa09
Jessica <145709183+outrasilva@users.noreply.github.com>
2024-05-27 10:57:08 -0300

    Delete WebSites/EcoSaver.Site directory
    Diretório não faz parte do main.

--- WebSites/EcoSaver.Site/scripts/footer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const navbarHTML = `
-    <footer class="rodape">
-        <p>Projeto 01 - Realizado pelo Grupo B - An&aacute;lise e Desenvolvimento de Sistemas - PUC Minas</p>
-    </footer>
-    `;
-
-    document.body.insertAdjacentHTML("afterbegin", navbarHTML);
-});
