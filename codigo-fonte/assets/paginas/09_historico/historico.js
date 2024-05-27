9bdaa126f29baa3384f6d3cd11894691640d1f01
outrasilva <jessica.1702@hotmail.com>
2024-05-27 10:54:05 -0300

    JS Tela Histórico

---
+++ codigo-fonte/assets/paginas/09_historico/historico.js
@@ -0,0 +1,23 @@
+let ordemCrescente = true;
+
+function alterarOrdem() {
+    const tabela = document.querySelector('.tabela tbody');
+    const linhas = Array.from(tabela.querySelectorAll('tr'));
+    const parseDate = str => {
+        const [day, month, year] = str.split('/').map(Number);
+        return new Date(year, month - 1, day);
+};
+
+linhas.sort((a, b) => {
+    const dataA = parseDate(a.cells[0].textContent.trim());
+    const dataB = parseDate(b.cells[0].textContent.trim());
+    return ordemCrescente ? dataA - dataB : dataB - dataA;
+});
+
+ordemCrescente = !ordemCrescente;
+
+linhas.forEach(linha => tabela.appendChild(linha));
+
+const ordenacaoIcon = document.querySelector('.ordenacao');
+ordenacaoIcon.innerHTML = ordemCrescente ? '&#9650;' : '&#9660;';
+}
